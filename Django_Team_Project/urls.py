2278b436d4245fa35e16ef233eaea4d4f1f067a4
hyunah <hil02165@naver.com>
2023-04-11 16:12:57 +0900

    create user app

--- Django_Team_Project/urls.py
+++ Django_Team_Project/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tweet.urls')),  # tweet app urls 연결
+    path('', include('user.urls')),  # user app urls 연결
 ]
