eec8119588095e8d7988786588ae287e9089470b
SungSuSon <thstjdtn05@gmail.com>
2023-04-11 20:48:20 +0900

    update CRUP_POST funtion,html

--- tweet/views.py
+++ tweet/views.py
@@ -13,27 +13,26 @@ def home(request):
 
     return render(request, "tweet/home.html")
 
-#게시글 작성
+#게시글 작성 ,login_required를 사용하는대신, 사용자를 로그인 페이지로 이동시킨다.
 def create_post(request):
     #접근한 사용자가 로그인한 유저가 아니라면 로그인 페이지로 이동한다.
     user = request.user.is_authenticated
     if not user:
         return redirect(reverse('sign-in'))
-
-    #HTML 출력
+    # GET : 글 작성 페이지 이동
     if request.method == 'GET':
         return render(request,'tweet/create_post.html')
 
-    # 데이터 수집
+    # 데이터 수집 및 저장
     elif request.method == 'POST':
         url = request.POST.get('url','')
         title = request.POST.get('title','')
         comment = request.POST.get('comment','')
-        owner = auth.get_user(request).user_id # son
+        owner = auth.get_user(request).user_id
 
         #접근한 유저가 UserModel에 등록된 사용자가 아닐경우 방지
         try :
-            owner = UserModel.objects.get(user_id=owner) # 오브젝트 포링키에 있는지
+            owner = UserModel.objects.get(user_id=owner)
         except UserModel.DoesNotExist:
             return redirect('/')
 
@@ -41,48 +40,26 @@ def create_post(request):
         if not all([url,title,comment]):
             return render(request, 'tweet/create_post.html',{'error':'빈칸 없이 입력해주세요.'})
 
-        # 게시글 저장, # usermodeml 연결한 값 : user _id
-
-        new_post = Post.objects.create(user_id=owner,url=url,title=title,comment=comment)
-        # # 나의 게시글 목록 가져오기, 정렬은 수정 순서가 아닌 생성시간 기준
-        my_post = Post.objects.filter(user_id=owner).order_by('-create_at')
+        # 게시글 저장,
+        new_post = Post.objects.create(owner=owner,url=url,title=title,comment=comment)
 
         # 게시글 저장후, 상세페이지로 이동
         return render(request,'tweet/create_post.html')
-        # return render(request,'상세페이지.html',{'my_post':my_post})
+        # return redirect(reverse('상세페이지'))
 
 # 게시글 수정
 def set_post(request,post_id):
-    test = Post.objects.all()
-    for i in test:
-        print(i.post_id)
-
-    print(post_id)
-
     #게시글이 없을 경우 방지
     try:
         post = Post.objects.get(post_id=post_id)
     except Post.DoesNotExist:
         return redirect('/')
-    # 지급 현재 작업영역 계십니다.
-    # 저장소에 최신 정보가 업데이트됬습니다.
 
-    # 현재 브렌치에서 커밋하시고
-    # 마스터로 체크아웃
-    # 풀로 최신 정보 업데이트하시고
-    # 브렌치랑 마스터 병합시고
-    # 커밋하시고
-    # 브렌치만들어서 작업
-
-    print("통과!")
     #작성가 아닌 유저의 접근 방지
     user = auth.get_user(request)
-    print(user.user_id) # 이름 user name
-    print(post.user_id) # id
-    print(post.user_id.user_id) # id
-    if post.user_id !=  user.user_id:
+    if post.owner.user_id !=  user.user_id:
         return redirect('/')
-    print("두번째 통과!")
+
     # GET요청의경우, 이전에 작성해둔 post의 데이터와 함께 html을 출력한다.
     if request.method == 'GET':
         return render(request, 'tweet/set_post.html', {'post':post})
@@ -94,34 +71,28 @@ def set_post(request,post_id):
         post.comment = request.POST.get('comment','')
         post.save()
 
-        # 자신이 작성한 포스트의, 수정 순서가 아닌 최신 게시글 작성 순서로 정렬한다.
-        post = Post.objects.filter(post_id=post_id).order_by('-create_at')
-        return redirect('/')
-        # return render(request, '상세페이지.html', {'post': post})
+        return render(request, 'tweet/set_post.html', {'post':post})
+        # return redirect(reverse('상세페이지'))
 
 # 게시글 삭제
-# @login_required
+# @login_required() 현재 유저와, owner의 id값을 비교하는 분기문이 있으므로 사용할 필요가 없으리라 기대한다.
 def delete_post(request,post_id):
     if request.method == 'GET':
         return redirect('/')
     elif request.method =='POST':
-
-        # 선택한 post_id의 id값을 읽어온다.
+        # 선택한 post_id의 id값을 읽어온다, 이미 삭제됬을 경우를 방지하여 try except를 사용한다.
         try:
-            owner = Post.objects.get(id = post_id)
+            post = Post.objects.get(post_id = post_id)
         except Post.DoesNotExist:
             return redirect('/')
 
         # 현재 접근한 유저와, 소유자의  id와 같은지 판별하고 데이터를 삭제한다.
-        if request.user.id == owner.id:
-            owner.delete()
-        else:
-            return redirect('/')
+        user = auth.get_user(request)
+        if user.user_id != post.owner.user_id:
+            post.delete()
 
-        # 삭제하고난 다음, 소유자의 모든 post를 조회한다.
-        post = Post.objects.filter(id=owner)
-        # return render(request,'마이 페이지.html',post)
-    return render(request, 'tweet/create_post.html')
+        return redirect('/')
+        # return redirect(reverse('상세페이지'))
 
 
 def my_page(request, user_id):
