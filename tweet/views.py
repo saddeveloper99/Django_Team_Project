b101b7424f14c91bced5b8b2db3cd9305b71fab6
Kim Gwang woon <misili304@naver.com>
2023-04-13 15:28:41 +0900

    C:/Program Files/Git/feat post_detail on the user profile page

--- tweet/views.py
+++ tweet/views.py
@@ -17,23 +17,23 @@ from ytmusicapi import YTMusic
 ytmusic = YTMusic()
 
 
-
-
 def home(request):
     all_post = Post.objects.all().order_by('-create_at')
     page = request.GET.get('page')
     paginator = Paginator(all_post, 8)
     page_obj = paginator.get_page(page)
-    
+
     try:
         page_obj = paginator.page(page)
     except:
         page = 1
         page_obj = paginator.page(page)
-    
-    return render(request, 'tweet/home.html', {'all_post': all_post, 'page_obj' : page_obj})
+
+    return render(request, 'tweet/home.html', {'all_post': all_post, 'page_obj': page_obj})
 
 # 게시글 작성 ,login_required를 사용하는대신, 사용자를 로그인 페이지로 이동시킨다.
+
+
 def create_post(request):
     # 접근한 사용자가 로그인한 유저가 아니라면 로그인 페이지로 이동한다.
     user = request.user.is_authenticated
@@ -50,35 +50,38 @@ def create_post(request):
         comment = request.POST.get('comment', '')
         owner = auth.get_user(request).user_id
         youtube_url = request.POST.get('youtube_url', '')
-        #접근한 유저가 UserModel에 등록된 사용자가 아닐경우 방지
-        try :
+        # 접근한 유저가 UserModel에 등록된 사용자가 아닐경우 방지
+        try:
             owner = UserModel.objects.get(user_id=owner)
         except UserModel.DoesNotExist:
             return redirect('/')
 
         # youtube url 데이터 검사
-        if 'youtube.com' in youtube_url :
+        if 'youtube.com' in youtube_url:
             youtube_url_check = youtube_url.split('watch?v=')[1][:11]
-        elif 'youtu.be' in youtube_url :
+        elif 'youtu.be' in youtube_url:
             youtube_url_check = youtube_url.split('be/')[1]
-        else :
+        else:
             return render(request, 'tweet/create_post.html', {'error': '유튜브 주소창을 입력해주세요.'})
-        
+
         # youtube 썸네일 주소 저장
-        youtube_thumbnail = ytmusic.get_song(youtube_url_check)['videoDetails']['thumbnail']['thumbnails'][0]['url']
-        
+        youtube_thumbnail = ytmusic.get_song(youtube_url_check)[
+            'videoDetails']['thumbnail']['thumbnails'][0]['url']
 
         # 데이터 검사
         if not all([title, comment]):
             return render(request, 'tweet/create_post.html', {'error': '빈칸 없이 입력해주세요.'})
         # 게시글 저장,
-        new_post = Post.objects.create(owner=owner,title=title,comment=comment,youtube_url=youtube_url_check, youtube_thumbnail=youtube_thumbnail)
+        new_post = Post.objects.create(owner=owner, title=title, comment=comment,
+                                       youtube_url=youtube_url_check, youtube_thumbnail=youtube_thumbnail)
         post_id = new_post.post_id
         # 게시글 저장후, 상세페이지로 이동
-        return redirect(reverse('post-detail',args=[post_id]))
+        return redirect(reverse('post-detail', args=[post_id]))
 
 # 게시글 수정
-def set_post(request,post_id):
+
+
+def set_post(request, post_id):
     # 탐색하는 게시글이 없을 경우 방지
     try:
         post = Post.objects.get(post_id=post_id)
@@ -97,10 +100,10 @@ def set_post(request,post_id):
     elif request.method == 'POST':
         post.title = request.POST.get('title', '')
         post.comment = request.POST.get('comment', '')
-        post.youtube_url = request.POST.get('youtube_url','')
+        post.youtube_url = request.POST.get('youtube_url', '')
         post.save()
         # 수정된 상세 페이지로 이동
-        return redirect(reverse('post-detail',args=[post_id]))
+        return redirect(reverse('post-detail', args=[post_id]))
 
 
 # 게시글 삭제
@@ -129,49 +132,60 @@ def my_page(request, user_id):
     if request.method == "GET":
         # id로 선택한 유저의 정보를 가져옴
         click_user = UserModel.objects.get(user_id=user_id)
-        # 유저 id가 post의 owner(fk)인 post를 가져와서
-        # 사용자 user(me), 프로필 주인(click_user)가 같을때만 수정버튼 html에 
         me = request.user
-
-        post = Post.objects.filter(owner=click_user).order_by('create_at')
+        # 유저 id가 post의 owner(fk)인 post를 가져와서
+        # 사용자 user(me), 프로필 주인(click_user)가 같을때만 수정버튼 html에
+        user_post = Post.objects.filter(owner=click_user).order_by('create_at')
+       
+        page = request.GET.get('page')
+        paginator = Paginator(user_post, 8)
+        page_obj = paginator.get_page(page)
         context = {
-            'click_user': click_user,
-            'posts': post,
-            'me' : me,
+                    'click_user': click_user,
+                    'me': me,
+                    'all_post': user_post, 
+                    'page_obj': page_obj,
         }
+        try:
+            page_obj = paginator.page(page)
+        except:
+            page = 1
+            page_obj = paginator.page(page)
+
         return render(request, 'tweet/my_page.html', context)
 
 
-def set_profile(request,user_id):
+def set_profile(request, user_id):
     # 메소드가 post일때, 유저네임, 프로필 사진, 프로필 메시지를 폼으로 받아온다.
     # 그리고 갱신하고 저장.
     if request.method == "POST":
         user = UserModel.objects.get(pk=user_id)
         me = request.user
-        
+
         try:
             profile_image = request.FILES['profile-image']
-            profile_image.name = 'user' + str(user.user_id) + '_' + str(random.randint(10000,100000)) + '.' + str(profile_image.name.split('.')[-1])
-                    
+            profile_image.name = 'user' + str(user.user_id) + '_' + str(
+                random.randint(10000, 100000)) + '.' + str(profile_image.name.split('.')[-1])
+
             # 파일 저장
             file_system_storage = FileSystemStorage()
             fs = file_system_storage.save(profile_image.name, profile_image)
-            
+
             # 저장한 파일 url 따기
             uploaded_file_url = file_system_storage.url(fs)
-            
+
             user.image = uploaded_file_url
         except:
             pass
 
         new_username = request.POST.get('username', '')
-        
-        if get_user_model().objects.filter(username=new_username).exists() and new_username != user.username: 
-            return render(request, 'tweet/set_profile.html', {'user': user, 'error':'이미 존재하는 사용자명입니다.'})
-        elif new_username == '' :
-            return render(request, 'tweet/set_profile.html', {'user': user, 'error':'수정할 이름을 입력해주세요.'})
+
+        if get_user_model().objects.filter(username=new_username).exists() and new_username != user.username:
+            return render(request, 'tweet/set_profile.html', {'user': user, 'error': '이미 존재하는 사용자명입니다.'})
+        elif new_username == '':
+            return render(request, 'tweet/set_profile.html', {'user': user, 'error': '수정할 이름을 입력해주세요.'})
         elif new_username.strip() == '':
-            return render(request, 'tweet/set_profile.html', {'user': user, 'error':'닉네임은 공백만으로 지을 수 없습니다.'})
+            return render(request, 'tweet/set_profile.html', {'user': user, 'error': '이름은 공백만으로 지을 수 없습니다.'})
         else:
             user.username = new_username
             user.description = request.POST.get('description', '')
@@ -199,9 +213,8 @@ def post_detail(request, post_id):
             return redirect('/')
 
         post_comments = PostCommentModel.objects.filter(post_id=post_id)
-        context = {'post':post,'post_comments':post_comments}
+        context = {'post': post, 'post_comments': post_comments}
 
-        return render(request, 'tweet/post_detail.html',context) #post_id를 받아와서 게시글 클릭하면 상세페이지로
+        # post_id를 받아와서 게시글 클릭하면 상세페이지로
+        return render(request, 'tweet/post_detail.html', context)
         # 테스트 주석
-
-
