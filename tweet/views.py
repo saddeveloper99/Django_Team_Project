4e63fb9a0461d08cee223321f2e5a1d910073e37
Kim Gwang woon <misili304@naver.com>
2023-04-13 10:22:13 +0900

    refac/ my_page.html, set_profile.html, tweet/views.py

--- tweet/views.py
+++ tweet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 from django.contrib import auth
 from .models import Post
 from user.models import UserModel
@@ -124,52 +125,63 @@ def my_page(request, user_id):
         # id로 선택한 유저의 정보를 가져옴
         click_user = UserModel.objects.get(user_id=user_id)
         # 유저 id가 post의 owner(fk)인 post를 가져와서
-        # test = Post.objects.all()
-        # print(test[0].title)
-        print(click_user)
+        # 사용자 user(me), 프로필 주인(click_user)가 같을때만 수정버튼 html에 
+        me = request.user
+
         post = Post.objects.filter(owner=click_user).order_by('create_at')
         context = {
             'click_user': click_user,
             'posts': post,
+            'me' : me,
         }
         return render(request, 'tweet/my_page.html', context)
 
 
 def set_profile(request,user_id):
-
     # 메소드가 post일때, 유저네임, 프로필 사진, 프로필 메시지를 폼으로 받아온다.
     # 그리고 갱신하고 저장.
     if request.method == "POST":
         user = UserModel.objects.get(pk=user_id)
         me = request.user
-
-        profile_image = request.FILES['profile-image']
-        profile_image.name = 'user' + str(user.user_id) + '_' + str(random.randint(10000,100000)) + '.' + str(profile_image.name.split('.')[-1])
+        
+        try:
+            profile_image = request.FILES['profile-image']
+            profile_image.name = 'user' + str(user.user_id) + '_' + str(random.randint(10000,100000)) + '.' + str(profile_image.name.split('.')[-1])
                     
-        # 파일 저장
-        file_system_storage = FileSystemStorage()
-        fs = file_system_storage.save(profile_image.name, profile_image)
-        
-        # 저장한 파일 url 따기
-        uploaded_file_url = file_system_storage.url(fs)
-        
-        user.image = uploaded_file_url
-        user.username = request.POST.get('username', '')
-        user.description = request.POST.get('description', '')
-        user.save()
+            # 파일 저장
+            file_system_storage = FileSystemStorage()
+            fs = file_system_storage.save(profile_image.name, profile_image)
+            
+            # 저장한 파일 url 따기
+            uploaded_file_url = file_system_storage.url(fs)
+            
+            user.image = uploaded_file_url
+        except:
+            pass
 
-        post = Post.objects.filter(owner=user).order_by('create_at')
-        context = {
-            'click_user': user,
-            'posts': post,
-        }
-        return render(request, 'tweet/my_page.html', context)
+        new_username = request.POST.get('username', '')
+        
+        if get_user_model().objects.filter(username=new_username).exists() and new_username != user.username: 
+            return render(request, 'tweet/set_profile.html', {'user': user, 'error':'이미 존재하는 사용자명입니다.'})
+        elif new_username == '':
+            return render(request, 'tweet/set_profile.html', {'user': user, 'error':'수정할 이름을 입력해주세요.'})
+        else:
+            user.username = new_username
+            user.description = request.POST.get('description', '')
+            user.save()
+            post = Post.objects.filter(owner=user).order_by('create_at')
+            context = {
+                'click_user': user,
+                'posts': post,
+                'me': me,
+            }
+            return render(request, 'tweet/my_page.html', context)
 
     # get일때는, 유저 정보를 id로 받아와서, 수정창에 입력 돼 있게 하기.
     elif request.method == "GET":
         user = UserModel.objects.get(user_id=request.user.user_id)
+        print(user.description)
         return render(request, 'tweet/set_profile.html', {'user': user})
-
 
 def post_detail(request, post_id):
     if request.method == 'GET':
