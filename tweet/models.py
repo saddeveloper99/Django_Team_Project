ea826b754a7b403ca88565646e5351eaad3df0fe
Churnobyl <tjdcjfals@gmail.com>
2023-04-13 15:27:27 +0900

    enhan/ 전체적인 프론트엔드 수정

--- tweet/models.py
+++ tweet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user.models import UserModel
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Post(models.Model):
@@ -14,4 +15,4 @@ class Post(models.Model):
     like_count = models.PositiveIntegerField(default=0)
     youtube_url = models.CharField(max_length=20)
     youtube_thumbnail = models.CharField(max_length=100)
-    avg_star = models.PositiveIntegerField(default=0)
+    avg_star = models.FloatField(default=0, validators=[MinValueValidator(0,5),MaxValueValidator(5.0)])
